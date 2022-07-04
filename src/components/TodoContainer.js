3647d46cb39599b2a805a568c1a34cd85ea4b8a4
David Vera Castillo <indigodavid@gmail.com>
2022-07-04 12:53:49 -0500

    Tutorial Part 1: Setup
    In this commit, the following files have been added:
    
    - TodoContainer: Class Component and state manager
    - TodosList: Class Component, todos list manager
    - TodoItem: Functional Component, list element renderer
    - Header: Functional Component, header renderer.
    - InputTodo and Navbar: These are empty modules at the moment

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import TodosList from './TodosList';
+
+class TodoContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+  }
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <div>
+        <Header />
+        <TodosList todos={todos} />
+      </div>
+    );
+  }
+}
+
+export default TodoContainer;
