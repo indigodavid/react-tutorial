f1888f0ef1053d29f3404a516d37858461896eda
David Vera Castillo <indigodavid@gmail.com>
2022-07-05 10:57:07 -0500

    Tutorial Part 9: Route with React Router

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/about',
+      text: 'About',
+    },
+  ];
+  // const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <nav className="NavBar">
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path}>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
