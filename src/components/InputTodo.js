c5e8ec5cc866058a481c5578ab0bb765f5a5fba1
David Vera Castillo <indigodavid@gmail.com>
2022-07-04 22:43:12 -0500

    Refactor InputTodo as functional component

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,51 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class InputTodo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-    };
-  }
+const InputTodo = (props) => {
+  const [inputText, setInputText] = useState({
+    title: '',
+  });
 
-  onChange = (e) => {
-    this.setState({
+  const onChange = (e) => {
+    setInputText({
+      ...inputText,
       [e.target.name]: e.target.value,
     });
-  }
+  };
 
-  handleSubmit = (e) => {
-    const { addTodoProps } = this.props;
-    const { title } = this.state;
+  const handleSubmit = (e) => {
+    const { addTodoProps } = props;
     e.preventDefault();
-    if (title.trim()) {
-      addTodoProps(title);
-      this.setState({
+    if (inputText.title.trim()) {
+      addTodoProps(inputText.title);
+      setInputText({
         title: '',
       });
     } else {
       alert('Input field should not be empty.');
     }
-  }
+  };
 
-  render() {
-    const { title } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          type="text"
-          placeholder="Add Todo..."
-          className="input-text"
-          name="title"
-          value={title}
-          onChange={this.onChange}
-        />
-        <button type="submit" className="input-submit">Submit</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        placeholder="Add Todo..."
+        className="input-text"
+        name="title"
+        value={inputText.title}
+        onChange={onChange}
+      />
+      <button type="submit" className="input-submit">Submit</button>
+    </form>
+  );
+};
 
 InputTodo.propTypes = {
   addTodoProps: PropTypes.func.isRequired,
