ff2ffe36c7aad352b626024a15c7310a40dc0164
David Vera Castillo <indigodavid@gmail.com>
2022-07-04 22:44:20 -0500

    Tutorial Part 7: React hooks

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,27 +1,25 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-class TodosList extends PureComponent {
-  render() {
-    const {
-      todos, handleChangeProps, deleteTodoProps, setUpdate,
-    } = this.props;
-    return (
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={handleChangeProps}
-            deleteTodoProps={deleteTodoProps}
-            setUpdate={setUpdate}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const TodosList = (props) => {
+  const {
+    todos, handleChangeProps, deleteTodoProps, setUpdate,
+  } = props;
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+          setUpdate={setUpdate}
+        />
+      ))}
+    </ul>
+  );
+};
 
 TodosList.propTypes = {
   todos: PropTypes.arrayOf(
