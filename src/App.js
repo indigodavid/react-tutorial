31bc7a88d85f88f0bb6ec1c8e8e3f4354766395e
David Vera Castillo <indigodavid@gmail.com>
2022-07-05 10:56:06 -0500

    Add routes and Navbar to main App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
+import About from './components/About';
+import NoMatch from './components/NoMatch';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <TodoContainer />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
   );
 }
 
