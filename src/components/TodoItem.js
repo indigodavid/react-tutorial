e986b6590149cecbf5df5bc952a99c3638d13a64
David Vera Castillo <indigodavid@gmail.com>
2022-07-04 20:33:44 -0500

    Tutorial Part 4: Edit todos items feature

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './TodoItem.module.css';
 
 const TodoItem = (props) => {
-  const { todo, handleChangeProps, deleteTodoProps } = props;
+  const {
+    todo, handleChangeProps, deleteTodoProps, setUpdate,
+  } = props;
+  const [editing, setEditing] = useState(false);
+
   const completedStyle = {
     fontStyle: 'italic',
     color: '#595959',
@@ -11,23 +15,52 @@ const TodoItem = (props) => {
     textDecoration: 'line-through',
   };
 
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const handleUpdatedDone = (e) => {
+    if (e.key === 'Enter') {
+      setEditing(false);
+    }
+  };
+
+  const viewMode = {};
+  const editMode = {};
+
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
   return (
     <li className={styles.item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          className={styles.checkbox}
+          onChange={() => handleChangeProps(todo.id)}
+        />
+        <button
+          type="button"
+          onClick={() => deleteTodoProps(todo.id)}
+        >
+          Delete
+        </button>
+        <span style={todo.completed ? completedStyle : null}>
+          {todo.title}
+        </span>
+      </div>
       <input
-        type="checkbox"
-        checked={todo.completed}
-        className={styles.checkbox}
-        onChange={() => handleChangeProps(todo.id)}
+        type="text"
+        className={styles.textInput}
+        style={editMode}
+        value={todo.title}
+        onChange={(e) => setUpdate(e.target.value, todo.id)}
+        onKeyDown={handleUpdatedDone}
       />
-      <button
-        type="button"
-        onClick={() => deleteTodoProps(todo.id)}
-      >
-        Delete
-      </button>
-      <span style={todo.completed ? completedStyle : null}>
-        {todo.title}
-      </span>
     </li>
   );
 };
@@ -40,6 +73,7 @@ TodoItem.propTypes = {
   }).isRequired,
   handleChangeProps: PropTypes.func.isRequired,
   deleteTodoProps: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
